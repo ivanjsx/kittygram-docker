751683e5784f7b0a4d727f2aaec5ca14ae313a69
Ivan Shanin <ivanxshanin@gmail.com>
2023-12-03 01:31:31 +0100

    rename everything back to comply with tests

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -36,7 +36,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'backend.urls'
+ROOT_URLCONF = 'kittygram_backend.urls'
 
 TEMPLATES = [
     {
@@ -54,7 +54,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'backend.wsgi.application'
+WSGI_APPLICATION = 'kittygram_backend.wsgi.application'
 
 
 DATABASES = {
